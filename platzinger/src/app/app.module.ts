baf840ccd24d211ec64feeae4932b3efce6d732e
Gabuvi7 <uviedo_gabriel@hotmail.com>
2019-05-28 00:35:11 -0300

    Nav entre componentes y aplicacion de routerModule

--- platzinger/src/app/app.module.ts
+++ platzinger/src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ConversationComponent } from './conversation/conversation.component';
+import { ProfileComponent } from './profile/profile.component';
+
+// Importar el appRoutes.
+import { Routes, RouterModule } from '@angular/router'; 
+
+// Declaracion de rutas
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent}, // Lo dejo vacio porque si la ruta no matchea con el path, lo lleva al HomeComponent
+  {path: 'home', component: HomeComponent}, // Ruta para home
+  {path: 'login', component: LoginComponent},
+  {path: 'conversation', component: ConversationComponent},
+  {path: 'profile', component: ProfileComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    ConversationComponent,
+    ProfileComponent
   ],
-  imports: [
+  imports: [ // El imports extiende funcionalidades de la app.
     BrowserModule,
-    AppRoutingModule
+    // Importar el RouterModule. No viene por defecto.
+    RouterModule.forRoot(appRoutes) // Le agreda caracteristicas de navegacion. Recibe las rutas y escucha si existe alguna modificacion en las rutas.
   ],
   providers: [],
   bootstrap: [AppComponent]
