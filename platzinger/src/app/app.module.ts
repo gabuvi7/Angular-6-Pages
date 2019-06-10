0c4cc21991d8557342d7c5ae69aa031a4e6b5980
Gabuvi7 <gabriel.uviedo@sarsy-srl.com>
2019-06-10 00:15:37 -0300

    38 - Instalación y setup de la librería AngularFire a través de npm
    Para conectar nuestro proyecto a los servicios de Firebase, usamos AngularFire, disponible en los repositorios de paquetes de npm. Al crear un proyecto en Firebase se nos muestran varias opciones de configuración. La opción de configuración web es la que vamos a utilizar, al obtener los datos y credenciales de autenticación las copiamos en el archivo environment.ts y en environment.prod.ts para que estén disponibles tanto en el ambiente de desarrollo como en producción.
    
    Una vez creadas las variables de configuración será necesario importar los módulos AngularFireModule y environment en app.modules.ts. Finalmente incluímos las clases de Firebase que usaremos en nuestro proyecto en la sección imports: AngularFireAuthModule, AngularFireStorageModule y AngularFireDatabaseModule.

--- platzinger/src/app/app.module.ts
+++ platzinger/src/app/app.module.ts
@@ -14,7 +14,15 @@ import { MenuComponent } from './menu/menu.component';
 
 import { SearchPipe } from './pipes/search';
 
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+
+import { environment } from '../environments/environment';
+import { AngularFireModule, FirebaseOptionsToken } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
 
 // Declaracion de rutas
 const appRoutes: Routes = [
@@ -25,7 +33,21 @@ const appRoutes: Routes = [
   {path: 'profile', component: ProfileComponent}
 ];
 
+/*Type '{ ngModule: typeof AngularFireModule; providers: { provide: InjectionToken<string | FirebaseAppConfig>; useValue: string | FirebaseAppConfig; }[]; }' is not assignable to type 'any[] | Type<any> | ModuleWithProviders<{}>'.
+  Property 'FormsModule' is missing in type '{ ngModule: typeof AngularFireModule; providers: { provide: InjectionToken<string | FirebaseAppConfig>; useValue: string | FirebaseAppConfig; }[]; }' but required in type 'ModuleWithProviders<{}>'.
+*/
 @NgModule({
+  imports: [ // El imports extiende funcionalidades de la app.
+    BrowserModule,
+    // Importar el RouterModule. No viene por defecto.
+    RouterModule.forRoot(appRoutes), // Le agreda caracteristicas de navegacion. Recibe las rutas y escucha si existe alguna modificacion en las rutas.
+    FormsModule, // importar el formsmodule porque sino, no funciona.
+    AngularFireModule,//.initializeApp(environment.firebaseConfig), NO ME FUNCIONA CON INITIALIZEAPP.
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule, // imports firebase/storage only needed for storage features
+    AngularFireDatabaseModule
+  ],
   declarations: [
     AppComponent,
     LoginComponent,
@@ -35,13 +57,9 @@ const appRoutes: Routes = [
     MenuComponent,
     SearchPipe //incluimos el pipe creado
   ],
-  imports: [ // El imports extiende funcionalidades de la app.
-    BrowserModule,
-    // Importar el RouterModule. No viene por defecto.
-    RouterModule.forRoot(appRoutes), // Le agreda caracteristicas de navegacion. Recibe las rutas y escucha si existe alguna modificacion en las rutas.
-    FormsModule // importar el formsmodule porque sino, no funciona.
+  providers: [
+    { provide: FirebaseOptionsToken, useValue: environment.firebaseConfig }
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- platzinger/src/environments/environment.prod.ts
+++ platzinger/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBJ0IXQORHVVumZYovrq5w3unks_r0ZV_I",
+    authDomain: "platzinger-324fc.firebaseapp.com",
+    databaseURL: "https://platzinger-324fc.firebaseio.com",
+    projectId: "platzinger-324fc",
+    storageBucket: "platzinger-324fc.appspot.com",
+    messagingSenderId: "476070506800"
+  }
 };
