8d4801db476cfc6459e9557afcba2641508ebd51
gabuvi7 <38091688+gabuvi7@users.noreply.github.com>
2019-06-30 22:19:02 -0300

    Revert "Features movies crud"

--- moviesCRUD/src/app/app.component.ts
+++ moviesCRUD/src/app/app.component.ts
@@ -1,29 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import * as $ from 'jquery'; 
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   title = 'moviesCRUD';
-  query: string = '';
-  
-  public ngOnInit(){
-    
-    $(window).resize(function(){
-      var tam = $(window).width();
-    
-      if (tam <= 990){
-        $('#idContainerPrincipal').addClass('mw-100','pl-0','pr-0','mx-0');
-        $('#containerCard').addClass('mw-100','pl-0','pr-0','mx-0');
-      }
-      else{
-        $('#idContainerPrincipal').removeClass('mw-100','pl-0','pr-0','mx-0');
-        $('#containerCard').removeClass('mw-100','pl-0','pr-0','mx-0');
-      }
-    });
-  }
 }
