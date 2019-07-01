e3812ae063dd1caf0a739265b9567989b933706a
Gabuvi7 <gabriel.uviedo@sarsy-srl.com>
2019-07-01 00:35:50 -0300

    Error de ngModel en prod

--- moviesCRUD/src/app/app.module.ts
+++ moviesCRUD/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule  } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FormComponent } from './form/form.component';
 import { Route, RouterModule } from '@angular/router';
 import { HttpClientModule  } from '@angular/common/http';
-import { FormsModule  } from '@angular/forms';
+
 
 import { SearchPipe } from './pipes/search.pipe';
 
